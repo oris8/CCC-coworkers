949b08bb08c680275de3cb7c852041b44397918f
oris8 <eprnf21@gmail.com>
2024-07-24 11:50:30 +0000

    📦 chore: pretendard Variable 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const pretendardFont = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pretendardFont.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
       <h1 className="inline-block bg-gradient-to-r from-red-500 via-green-500 to-violet-500 bg-clip-text text-6xl text-transparent">
         Hello World
       </h1>
+      <div>
+        <p className="text-3xl font-thin">안녕하세요</p>
+        <p className="text-3xl font-extralight">안녕하세요</p>
+        <p className="text-3xl font-light">안녕하세요</p>
+        <p className="text-3xl font-normal">안녕하세요</p>
+        <p className="text-3xl font-medium">안녕하세요</p>
+        <p className="text-3xl font-semibold">안녕하세요</p>
+        <p className="text-3xl font-bold">안녕하세요</p>
+        <p className="text-3xl font-extrabold">안녕하세요</p>
+        <p className="text-3xl font-black">안녕하세요</p>
+      </div>
     </div>
   );
 }
