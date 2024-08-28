1ec1138a439bffa200897753932c6f922cf2fbbe
oris8 <eprnf21@gmail.com>
2024-08-28 09:42:31 +0000

    ♻️ refactor: pusherServe에 as string 삭제

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,17 +2,17 @@ import PusherServer from 'pusher';
 import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID as string,
-  key: process.env.NEXT_PUBLIC_PUSHER_KEY as string,
-  secret: process.env.PUSHER_SECRET as string,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY as string,
+  process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
     authEndpoint: '/api/pusher/auth',
     authTransport: 'ajax',
     auth: {
